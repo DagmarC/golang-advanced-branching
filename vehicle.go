6ab06be2686674a171b5f6dd1b2d02f145dd4a71
DagmarC <dg.cervenkova@gmail.com>
2021-09-12 11:43:21 +0200

    Completed module

--- vehicle.go
+++ vehicle.go
@@ -169,8 +169,8 @@ func showRating(model string) {
 		if m == model {
 			fmt.Printf("Total Ratings:%v\tPositive:%v\tNegative:%v\tNeutral:%v",
 				r.feedbackTotal, r.feedbackPositive, r.feedbackNegative, r.feedbackNeutral)
+			ratingFound = true
 		}
-		ratingFound = true
 	}
 	if !ratingFound {
 		fmt.Printf("No rating for this vehicle")
